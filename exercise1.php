a5b9416be426b3d15883a4dfbcf2cb6f57cd4556
trantiendat <tiendat09022k1@gmail.com>
2022-12-28 16:32:17 +0700

    PHP basic

---
+++ exercise1.php
@@ -0,0 +1,56 @@
+<?php
+
+function checkValidString($str)
+{
+  if (strpos($str, 'book') || strpos($str, 'restaurant')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// cau 1
+$cau1 = checkValidString('this is');
+if ($cau1 == true) {
+  echo "<p>Chuỗi hợp lệ</p>";
+} else {
+  echo "<p>Chuỗi ko hợp lệ</p>";
+}
+//cau 2
+$myfile1 = fopen("file1.txt", "r") or die("Unable to open file!");
+$content = fread($myfile1, filesize('file1.txt'));
+$cau21 = checkValidString($content);
+if ($cau21 == true) {
+  $sentences = explode('.', $content);
+  $num_sentences = count($sentences) - 1;
+  echo "<p>Chuỗi hợp lệ. Chuỗi bao gồm " . $num_sentences . " câu</p>";
+} else {
+  echo "<p>Chuỗi ko hợp lệ</p>";
+}
+fclose($myfile1);
+
+$myfile2 = fopen("file2.txt", "r") or die("Unable to open file!");
+$content1 = fread($myfile2, filesize('file2.txt'));
+$cau22 = checkValidString($content1);
+if ($cau22 == true) {
+  $sentences1 = explode('.', $content1);
+  $num_sentences1= count($sentences1) - 1;
+  echo "<p>Chuỗi hợp lệ. Chuỗi bao gồm " . $num_sentences1 . " câu</p>";
+} else {
+  echo "<p>Chuỗi ko hợp lệ</p>";
+}
+fclose($myfile2);
+?>
+
+<!-- <?php
+
+      function containsBook($str)
+      {
+        return strpos($str, 'book') !== false;
+      }
+
+      $containsBook = containsBook('This string contains the word book');
+      echo $containsBook; // In ra giá trị true
+
+      $containsBook = containsBook('This string does not contain the word book');
+      echo $containsBook; // In ra giá trị false
+      ?> -->
